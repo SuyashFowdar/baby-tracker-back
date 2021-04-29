d7829a581332ae8557718eb59f6dc59a714dda85
Suyash <suyash.krishna9@gmail>
2021-04-29 20:42:58 +0400

    Use default methods for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ require 'jwt'
 
 class UsersController < ApplicationController
   def create
-    new_user = JSON.parse(request.body.read)
+    new_user = users_params
     new_user['password'] = Digest::SHA2.new(512).hexdigest(new_user['password'])
     @user = User.new(new_user)
     if @user.save
@@ -12,31 +12,17 @@ class UsersController < ApplicationController
       token = JWT.encode(payload.to_json, secret_key, 'none')
       render json: { token: token }, status: :ok
     else
-      render json: { error: @user.errors }, status: :internal_server_error
+      error = if @user.errors.messages && @user.errors.messages[:name]
+                "Name #{@user.errors.messages[:name][0]}"
+              else
+                "Couldn't Sign Up"
+              end
+      render json: { error: error }, status: :unauthorized
     end
   end
 
-  def login
-    login_user = JSON.parse(request.body.read)
-    login_user['password'] = Digest::SHA2.new(512).hexdigest(login_user['password'])
-    @user = User.where(login_user)
-    if @user && @user[0]
-      payload = { 'id' => @user[0][:id], 'created_at' => @user[0][:created_at] }
-      token = JWT.encode(payload.to_json, secret_key, 'none')
-      render json: { token: token, name: @user[0]['name'] }, status: :ok
-    else
-      render json: { error: 'User not found!' }, status: :internal_server_error
-    end
-  end
-
-  def admin
-    payload = JWT.decode(request.headers['token'], secret_key, false)
-    @user = User.find(JSON.parse(payload[0])['id'])
-    if @user && @user.name == 'Admin'
-      render json: { admin: true }, status: :ok
-    else
-      render json: { admin: false }, status: :internal_server_error
-    end
+  def users_params
+    params.require(:user).permit(:name, :password)
   end
 
   private
