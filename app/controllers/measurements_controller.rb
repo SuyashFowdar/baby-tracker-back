d4ffb194316a3168cbdc4f9ee47600153442c33a
Suyash <suyash.krishna9@gmail>
2021-04-29 20:43:12 +0400

    Use default methods for measurements

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -5,23 +5,27 @@ class MeasurementsController < ApplicationController
   def create
     payload = JWT.decode(request.headers['token'], secret_key, false)
     @user = User.find(JSON.parse(payload[0])['id'])
-    @measurement = @user.measurements.build(JSON.parse(request.body.read))
+    @measurement = @user.measurements.build(measurements_params)
     if @measurement.save
       render json: { measurement: @measurement }, status: :ok
     else
-      render json: { error: 'Measurement not created' }, status: :internal_server_error
+      render json: { error: 'Measurement not created!' }, status: :unauthorized
     end
   end
 
-  def fetch
+  def index
     payload = JWT.decode(request.headers['token'], secret_key, false)
     @user = User.find(JSON.parse(payload[0])['id'])
     @measures = Measure.all
     if @user
       render json: { measurements: @user.measurements, measures: @measures }, status: :ok
     else
-      render json: { error: 'Measurement not created' }, status: :internal_server_error
+      render json: { error: 'Measurements not fetched!' }, status: :unauthorized
     end
+  end
+
+  def measurements_params
+    params.require(:measurement).permit(:amount, :measure_id)
   end
 
   private
