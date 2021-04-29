2a87d125e76b71b72fae8244a4cb384e42908460
Suyash <suyash.krishna9@gmail>
2021-04-29 20:44:01 +0400

    Use resources for routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get 'users/all'
-  # get 'users/:id', to: 'users#show'
-  post 'users/create'
-  post 'users/login'
-  get 'users/admin'
-  post 'measures/create'
-  post 'measurements/create'
-  get 'measurements/fetch'
-  root 'users#index'
+  resources :users, only: [:create]
+  resources :sessions, only: [:create]
+  resources :admins, only: [:index]
+  resources :measurements, only: [:create, :index]
+  resources :measures, only: [:create]
 end
