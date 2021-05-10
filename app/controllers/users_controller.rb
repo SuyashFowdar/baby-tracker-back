d22beed0622d140f20e7e3e2eacdce90a12ef512
Suyash <suyash.krishna9@gmail>
2021-05-10 18:26:09 +0400

    Send errors properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,7 @@ class UsersController < ApplicationController
     if @user.save
       render json: { token: encode_token }, status: :ok
     else
-      error = if @user.errors.messages && @user.errors.messages[:name]
-                "Name #{@user.errors.messages[:name][0]}"
-              else
-                "Couldn't Sign Up"
-              end
-      render json: { error: error }, status: :unauthorized
+      render json: { error: "Couldn't Sign Up", details: @user.errors }, status: :unprocessable_entity
     end
   end
 
